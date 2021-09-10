7ae02be85d6164fa650daffbb9a2577f94e4d1af
Zensify <78502455+Zensify@users.noreply.github.com>
2021-09-09 23:46:20 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,80 +1,94 @@
-#Pygame Import
 import pygame
+
+# Setup
 pygame.init()
 
-#CONSTANTS
-BLACK = (0  , 0  , 0 )
-WHITE = (255, 255, 255)
+# Colors
+color_black = (0  , 0  , 0 )
+color_white = (255, 255, 255)
  
-#Window 
+# Window 
 size = (700, 500)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Pong")
- 
-#End Loop
-done = False
- 
-#Refresh rate
 clock = pygame.time.Clock()
+fps = 60
 
-# --- Square ---
+# --- Ball ---
 
-#Position
+# Position
 rect_x = 50
 rect_y = 50
 
-#Speed
+# Speed
 rect_change_x = 5
 rect_change_y = 5
 
-# --- Paddle ---
+# --- Paddle 1 ---
 
-#Position 
-paddle_x =  25
-paddle_y =  350
+# Position 
+paddle1_x =  25
+paddle1_y =  200
 
-#Speed
-paddle_change_y = 5
+# Speed
+paddle1_change_y = 5
+
+# --- Paddle 2 ---
+
+# Position 
+paddle2_x =  655
+paddle2_y =  200
+
+# Speed
+paddle2_change_y = 5
 
 # -------- Main Program Loop -----------
-while not done:
+while True:
+    clock.tick(fps)
+  
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            done = True
+           pygame.quit
  
-    #Background color
-    screen.fill(BLACK)
+    # Background color
+    screen.fill(color_black)
 
-    # --- Square ---
-    pygame.draw.rect(screen, WHITE, [rect_x, rect_y, 35, 35])
+    # --- Ball ---
+    pygame.draw.rect(screen, color_white, [rect_x, rect_y, 35, 35])
     rect_x += rect_change_x
     rect_y += rect_change_y
 
-    #Bounce
+    # Bounce
     if rect_y > 450 or rect_y < 0:
         rect_change_y = rect_change_y * -1
     if rect_x > 650 or rect_x < 0: 
         rect_change_x = rect_change_x * -1
 
-    # --- Paddle --- 
-    pygame.draw.rect(screen, WHITE, [paddle_x, paddle_y, 15, 110])
+    # --- Paddle 1 --- 
+    pygame.draw.rect(screen, color_white, [paddle1_x, paddle1_y, 15, 110])
 
-    #Controls
+    # Controls
     keys = pygame.key.get_pressed()
-    paddle_y += (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * paddle_change_y
+    paddle1_y += (keys[pygame.K_s] - keys[pygame.K_w]) * paddle1_change_y
 
-    #Clamp Paddle to Play Area
-    if paddle_y < 0:
-        paddle_y = 0
-    if paddle_y > 400:
-        paddle_y = 400
+    # Clamp Paddle to Play Area
+    if paddle1_y < 0:
+        paddle1_y = 0
+    if paddle1_y > 400:
+        paddle1_y = 400
 
-    
-    #Refresh Screen
+    # --- Paddle 2 --- 
+    pygame.draw.rect(screen, color_white, [paddle2_x, paddle2_y, 15, 110])
+
+    # Controls
+    keys = pygame.key.get_pressed()
+    paddle2_y += (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * paddle2_change_y
+
+    # Clamp Paddle to Play Area
+    if paddle2_y < 0:
+        paddle2_y = 0
+    if paddle2_y > 400:
+        paddle2_y = 400
+      
+    # Refresh Screen
     pygame.display.flip()
- 
-    #FPS
-    clock.tick(60)
- 
-#Close Game
-pygame.quit()
